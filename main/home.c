87146089411a0a589c2b92f9d0491696395319ed
andrinwinzap <AndrinWinzap@proton.me>
2025-07-26 21:57:56 +0200

    add debug output

--- main/home.c
+++ main/home.c
@@ -22,7 +22,8 @@ bool reached_target_pos(axis_t *axis)
 void homing_task(void *pvParams)
 {
     homing_params_t *params = (homing_params_t *)pvParams;
-    enum
+
+    typedef enum
     {
         FINDING_A_AXIS_END_COARSE,
         BACKING_OFF_A_ENDSTOP,
@@ -33,56 +34,59 @@ void homing_task(void *pvParams)
         FINDING_B_AXIS_FALLING_EDGE,
         MOVING_TO_ZERO,
         FINISHED
-    } state = FINDING_A_AXIS_END_COARSE;
+    } homing_state_t;
+
+    homing_state_t state = FINDING_A_AXIS_END_COARSE;
+    homing_state_t last_logged_state = -1; // So we log the first state too
 
     int8_t dir;
-    float rising_edge;
-    float falling_edge;
+    float rising_edge, falling_edge;
 
     params->wrist->axis_a.speed_ctrl = COARSE_HOMING_SPEED;
+
     while (true)
     {
         if (xSemaphoreTake(params->homing_semaphore, portMAX_DELAY) == pdTRUE)
         {
+            if (state != last_logged_state)
+            {
+                ESP_LOGI(TAG, "State: %d", state);
+                last_logged_state = state;
+            }
 
             switch (state)
             {
             case FINDING_A_AXIS_END_COARSE:
-            {
                 if (a_axis_endstop())
                 {
                     params->wrist->axis_a.speed_ctrl = 0.0;
                     float coarse_pos = as5600_get_position(&params->wrist->axis_a.encoder);
                     params->wrist->axis_a.pos_ctrl = coarse_pos - 0.3;
-
+                    ESP_LOGI(TAG, "A endstop (coarse) hit at %.4f", coarse_pos);
                     state = BACKING_OFF_A_ENDSTOP;
                 }
                 break;
-            }
+
             case BACKING_OFF_A_ENDSTOP:
-            {
                 if (reached_target_pos(&params->wrist->axis_a))
                 {
                     params->wrist->axis_a.speed_ctrl = FINE_HOMING_SPEED;
-
                     state = FINDING_A_AXIS_END_FINE;
                 }
                 break;
-            }
+
             case FINDING_A_AXIS_END_FINE:
-            {
                 if (a_axis_endstop())
                 {
                     params->wrist->axis_a.speed_ctrl = 0.0;
                     as5600_set_position(&params->wrist->axis_a.encoder, ENDSTOP_A_POSITION);
-                    params->wrist->axis_a.pos_ctrl = -M_PI / 2.0f;
-
+                    params->wrist->axis_a.pos_ctrl = M_PI / 2 - 0.04;
+                    ESP_LOGI(TAG, "A endstop (fine) hit, zeroed to %.4f", ENDSTOP_A_POSITION);
                     state = MOVING_TO_B_END;
                 }
                 break;
-            }
+
             case MOVING_TO_B_END:
-            {
                 if (reached_target_pos(&params->wrist->axis_a))
                 {
                     float pos = as5600_get_position(&params->wrist->axis_b.encoder);
@@ -91,61 +95,52 @@ void homing_task(void *pvParams)
                     state = MOVING_NEXT_TO_B_END_SWITCH;
                 }
                 break;
-            }
 
             case MOVING_NEXT_TO_B_END_SWITCH:
-            {
                 if (reached_target_pos(&params->wrist->axis_b))
                 {
                     params->wrist->axis_b.speed_ctrl = FINE_HOMING_SPEED * dir;
                     state = FINDING_B_AXIS_RISING_EDGE;
                 }
                 break;
-            }
 
             case FINDING_B_AXIS_RISING_EDGE:
-            {
                 if (b_axis_endstop())
                 {
                     rising_edge = as5600_get_position(&params->wrist->axis_b.encoder);
-                    params->wrist->axis_b.speed_ctrl = FINE_HOMING_SPEED * dir;
-                    ESP_LOGI(TAG, "RISING EDGE: %.4f", rising_edge);
-
+                    ESP_LOGI(TAG, "B rising edge at %.4f", rising_edge);
                     state = FINDING_B_AXIS_FALLING_EDGE;
                 }
-
                 break;
-            }
 
             case FINDING_B_AXIS_FALLING_EDGE:
-            {
                 if (!b_axis_endstop())
                 {
                     params->wrist->axis_b.speed_ctrl = 0.0;
                     falling_edge = as5600_get_position(&params->wrist->axis_b.encoder);
-                    ESP_LOGI(TAG, "Falling EDGE: %.4f", falling_edge);
+                    ESP_LOGI(TAG, "B falling edge at %.4f", falling_edge);
 
-                    float calibrated_position = (falling_edge - rising_edge) / 2.0f;
-                    as5600_set_position(&params->wrist->axis_b.encoder, calibrated_position);
+                    float calibrated = (falling_edge - rising_edge) / 2.0f;
+                    as5600_set_position(&params->wrist->axis_b.encoder, calibrated);
 
-                    params->wrist->axis_b.pos_ctrl = 0.5f;
+                    params->wrist->axis_b.pos_ctrl = 0.0f;
                     params->wrist->axis_a.pos_ctrl = 0.0f;
 
                     state = MOVING_TO_ZERO;
                 }
                 break;
-            }
 
             case MOVING_TO_ZERO:
-            {
-                if ((reached_target_pos(&params->wrist->axis_a)) && (reached_target_pos(&params->wrist->axis_b)))
+                if (reached_target_pos(&params->wrist->axis_a) &&
+                    reached_target_pos(&params->wrist->axis_b))
                 {
+                    ESP_LOGI(TAG, "Homing complete. Both axes at zero.");
                     state = FINISHED;
                 }
                 break;
-            }
 
-            default:
+            case FINISHED:
+                // Optionally suspend task here
                 break;
             }
         }
