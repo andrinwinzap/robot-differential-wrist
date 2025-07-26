63d08bc7020c40ffcd15721794f3703f4b4e38f1
andrinwinzap <AndrinWinzap@proton.me>
2025-07-26 13:20:11 +0200

    maintain speed ratio in case of impossible speed

--- components/diff_speed_ctrl/src/diff_speed_ctrl.c
+++ components/diff_speed_ctrl/src/diff_speed_ctrl.c
@@ -1,13 +1,25 @@
 #include "diff_speed_ctrl.h"
+#include <math.h>
 
 void update_outputs(diff_speed_ctrl_t *ctrl)
 {
     float add_scaled = ctrl->common_speed / ctrl->gear_ratio;
 
+    float speed1 = add_scaled + ctrl->differential_speed;
+    float speed2 = add_scaled - ctrl->differential_speed;
+
+    float max_abs = fmaxf(fabsf(speed1), fabsf(speed2));
+
+    if (max_abs > 1)
+    {
+        speed1 /= max_abs;
+        speed2 /= max_abs;
+    }
+
     if (ctrl->cb1)
-        ctrl->cb1(add_scaled + ctrl->differential_speed);
+        ctrl->cb1(speed1);
     if (ctrl->cb2)
-        ctrl->cb2(add_scaled - ctrl->differential_speed);
+        ctrl->cb2(speed2);
 }
 
 void set_common_speed(diff_speed_ctrl_t *ctrl, float speed)
